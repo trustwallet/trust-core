a2f634624612557e1b7c00fa61d254d93128b142
Alejandro Isaza <al@isaza.ca>
2018-01-28 09:43:24 -0800

    Initial Commit

---
+++ TrustCore/TrustCore.h
@@ -0,0 +1,19 @@
+//
+//  TrustCore.h
+//  TrustCore
+//
+//  Created by Alejandro Isaza on 2018-01-28.
+//  Copyright © 2018 Trust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TrustCore.
+FOUNDATION_EXPORT double TrustCoreVersionNumber;
+
+//! Project version string for TrustCore.
+FOUNDATION_EXPORT const unsigned char TrustCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrustCore/PublicHeader.h>
+
+
