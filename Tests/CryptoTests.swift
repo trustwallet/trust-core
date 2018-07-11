35560fd8a2327494a05549999f89c7870701911b
Alejandro Isaza <al@isaza.ca>
2018-07-10 21:30:04 -0700

    Add missing tests

--- Tests/CryptoTests.swift
+++ Tests/CryptoTests.swift
@@ -23,4 +23,29 @@ class CryptoTests: XCTestCase {
         let privateKey = Data(hexString: "7a28b5ba57c53603b0b07b56bba752f7784bf506fa95edc395f5cf6c7514fe9d")!
         XCTAssertEqual(Crypto.getPublicKey(from: privateKey).hexString, "0432d87c5cd4b31d81c5b010af42a2e413af253dc3a91bd3d53c6b2c45291c3de71633bf7793447a0d3ddde601f8d21668fca5b33324f14ebe7516eab0da8bab8f")
     }
+
+    func testDeriveSeed() {
+        let mnemonic = "often tobacco bread scare imitate song kind common bar forest yard wisdom"
+        let passphrase = "testtest123"
+        let seed = Data(hexString: "b4186ab8ac0ebfd3c20f992d0b602639fe59f0e4d2e66dea487194580e0aa0031387c9f30488a7628ed7350a63dd97e1acb259896082e3b34a1ff0dd85c287d1")!
+
+        XCTAssertEqual(Crypto.deriveSeed(mnemonic: mnemonic, passphrase: passphrase), seed)
+    }
+
+    func testEncode() {
+        let message = "c61d43dc5bb7a4e754d111dae8105b6f25356492df5e50ecb33b858d94f8c338"
+        let expected = "ship tube warfare resist kid inhale fashion captain sustain dog bitter tattoo fashion rather enter type extend grain solve arch sun ladder artefact bronze"
+        let words = Crypto.generateMnemonic(seed: Data(hexString: message)!)
+        XCTAssertEqual(words, expected)
+    }
+
+    func testValid() {
+        let mnemonic = "ship tube warfare resist kid inhale fashion captain sustain dog bitter tattoo fashion rather enter type extend grain solve arch sun ladder artefact bronze"
+        XCTAssertTrue(Crypto.isValid(mnemonic: mnemonic))
+    }
+
+    func testInvalid() {
+        let mnemonic = "ship turd warfare resist kid inhale fashion captain sustain dog bitter tattoo fashion rather enter type extend grain solve arch sun ladder artefact bronze"
+        XCTAssertFalse(Crypto.isValid(mnemonic: mnemonic))
+    }
 }
