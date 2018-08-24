b42e79989f1e69eec2c02d1ea5e442e46fe4fa3c
Viktor Radchenko <yazexel@gmail.com>
2018-08-23 18:36:34 -0700

    Add WanchainTransaction

---
+++ Sources/Wanchain/WanchainTransaction.swift
@@ -0,0 +1,30 @@
+// Copyright © 2017-2018 Trust.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+import Foundation
+import BigInt
+
+struct WanchainTransactionType {
+    static let normal = BigInt(1)
+    static let privacy = BigInt(6)
+}
+
+public struct WanchainTransaction {
+    let type: WanchainTransactionType
+    var transaction: EthereumTransaction
+
+    /// Signs this transaction by filling in the `v`, `r`, and `s` values.
+    ///
+    /// - Parameters:
+    ///   - chainID: chain identifier, defaults to `1`
+    ///   - hashSigner: function to use for signing the hash
+    public mutating func sign(chainID: Int, hashSigner: (Data) throws -> Data) rethrows {
+        let signer = WanchainSigner(chainID: BigInt(chainID))
+        let hash = signer.hash(transaction: self)
+        let signature = try hashSigner(hash)
+        (transaction.r, transaction.s, transaction.v) = signer.values(signature: signature)
+    }
+}
