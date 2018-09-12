7d9a497debf436d28696d87ed9ea0aa3518e7c76
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2018-09-11 18:50:25 -0700

    Use bitcoinKeyHash

--- Sources/Bitcoin/PublicKey+Bitcoin.swift
+++ Sources/Bitcoin/PublicKey+Bitcoin.swift
@@ -10,7 +10,7 @@ public extension PublicKey {
     /// Returns the public key address with the given prefix.
     public func bitcoinAddress(prefix: UInt8) -> BitcoinAddress {
         let witnessVersion = Data([0x00] + [0x14])
-        let redeemScript = Crypto.sha256ripemd160(witnessVersion + Crypto.sha256ripemd160(data))
+        let redeemScript = Crypto.sha256ripemd160(witnessVersion + bitcoinKeyHash)
         let address = Crypto.base58Encode([prefix] + redeemScript)
         return BitcoinAddress(string: address)!
     }
