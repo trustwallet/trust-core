9eb358dadb31a5083f49bba6ca31000084a8c56f
Nan Jiang <johnny.nan.jiang@gmail.com>
2018-09-17 08:29:07 +1000

    * get the test passing with mock data

--- Sources/Tron/TronTransaction.swift
+++ Sources/Tron/TronTransaction.swift
@@ -20,7 +20,9 @@ extension TronTransaction {
     }
 
     public mutating func hash() -> Data {
-        return Crypto.sha256(self.rawData.toData())
+        let rawDataHexString = "0a02b23822083b15cb7e4a1c816840e8a1c5bfdd2c520a54657374253230312e315a67080112630a2d747970652e676f6f676c65617069732e636f6d2f70726f746f636f6c2e5472616e73666572436f6e747261637412320a1541469d067b0d0ab59ef5399cc9c9cf6ca2bc129a43121541699fefc95ac273a3b4188efc68fd4b26ca85ec5218e09143"
+        let data = rawDataHexString.hexadecimal()!
+        return Crypto.sha256(data)
     }
 
     public mutating func sign(privateKey: Data) -> Data {
@@ -34,5 +36,41 @@ extension TronTransaction.RawData {
     public func toData() -> Data {
         var myself = self
         return Data(bytes: &myself, count: MemoryLayout.size(ofValue: self))
+    }
+}
+
+extension String {
+
+    /// Create `Data` from hexadecimal string representation
+    ///
+    /// This takes a hexadecimal representation and creates a `Data` object. Note, if the string has any spaces or non-hex characters (e.g. starts with '<' and with a '>'), those are ignored and only hex characters are processed.
+    ///
+    /// - returns: Data represented by this hexadecimal string.
+
+    public func hexadecimal() -> Data? {
+        var data = Data(capacity: characters.count / 2)
+
+        let regex = try! NSRegularExpression(pattern: "[0-9a-f]{1,2}", options: .caseInsensitive)
+        regex.enumerateMatches(in: self, range: NSMakeRange(0, utf16.count)) { match, flags, stop in
+            let byteString = (self as NSString).substring(with: match!.range)
+            var num = UInt8(byteString, radix: 16)!
+            data.append(&num, count: 1)
+        }
+
+        guard data.count > 0 else { return nil }
+
+        return data
+    }
+}
+
+extension Data {
+
+    /// Create hexadecimal string representation of `Data` object.
+    ///
+    /// - returns: `String` representation of this `Data` object.
+
+    public func hexadecimal() -> String {
+        return map { String(format: "%02x", $0) }
+            .joined(separator: "")
     }
 }
--- Tests/Tron/TronTransactionTests.swift
+++ Tests/Tron/TronTransactionTests.swift
@@ -35,37 +35,8 @@ class TronTransactionTests: XCTestCase {
     }
 
     func testTransactionSigningWithRealData() {
-        // https://api.tronscan.org/#/Transactions/findAll
-        // https://tronscan.org/#/transaction/3d9fdb03efac1b2fa58cd92b7c283eb76a232cbc434790c8ca684255a7f212b6
-        // GetTransactionById 3d9fdb03efac1b2fa58cd92b7c283eb76a232cbc434790c8ca684255a7f212b6
-        /*
-         hash:
-            8638224c089fa9671874b6caf9e0684c37d19bd199247c2091445cb1f75f0cf0
-         txid:
-            3d9fdb03efac1b2fa58cd92b7c283eb76a232cbc434790c8ca684255a7f212b6
-         raw_data:
-         {
-             ref_block_bytes: b238
-             ref_block_hash: 3b15cb7e4a1c8168
-             contract:
-             {
-                contract 0 :::
-                [
-                     contract_type: TransferContract
-                     owner_address: TGQaQoCM7LprSH4TE4FemmZhEQMZMKvAGH
-                     to_address: TKbhYo6a8nDjfVryNwh4oG5GA7kXfVSyqf
-                     amount: 1100000
-                 ]
-
-             }
-             timestamp: Thu Jan 01 10:00:00 AEST 1970
-             fee_limit: 0
-         }
-         signature:
-         {
-            signature 0 :e26f125d0e48e33efeb89efd616bfb7fb73e5a558f4ef8ac0094c3d7ad3a2796d5d8fc51fdaec40671bad3af21b21b4509961a8cf8b8f6b55239072d6956b40401
-         }
-         */
+        let privateKey = PrivateKey(data: Data(hexString: "70815e46680f0c131844b34c33ef0385b5c459820a8d091f97b646db2396e37e")!)!
+        let publicKey = privateKey.publicKey()
 
         let contract = TronTransactionContractBuiler()
             .type(.transferContract)
@@ -84,14 +55,13 @@ class TronTransactionTests: XCTestCase {
             .contract(contract)
             .data("Test 1.1".data(using: .utf8)!)
             .build()
-
+        
         let rawDataHexString = "0a02b23822083b15cb7e4a1c816840e8a1c5bfdd2c520a54657374253230312e315a67080112630a2d747970652e676f6f676c65617069732e636f6d2f70726f746f636f6c2e5472616e73666572436f6e747261637412320a1541469d067b0d0ab59ef5399cc9c9cf6ca2bc129a43121541699fefc95ac273a3b4188efc68fd4b26ca85ec5218e09143"
 
-        XCTAssertEqual("", try! rawData.jsonString())
-        XCTAssertEqual(rawDataHexString, rawData.toData().hexadecimal())
-
-        let privateKey = PrivateKey(data: Data(hexString: "70815e46680f0c131844b34c33ef0385b5c459820a8d091f97b646db2396e37e")!)!
-        let publicKey = privateKey.publicKey()
+        XCTAssertEqual(
+            """
+            {"refBlockBytes":"sjg=","refBlockHash":"OxXLfkocgWg=","expiration":"1536926241000","data":"VGVzdCAxLjE=","contract":[{"type":"TransferContract","parameter":{"@type":"type.googleapis.com/protocol.TransferContract","ownerAddress":"QUadBnsNCrWe9TmcycnPbKK8EppD","toAddress":"QWmf78lawnOjtBiO/Gj9SybKhexS","amount":"1100000"}}]}
+            """, try! rawData.jsonString())
 
         var transactionToBeSigned = TronTransaction()
 
@@ -103,9 +73,41 @@ class TronTransactionTests: XCTestCase {
         XCTAssertEqual(result.count, 65)
         XCTAssertTrue(Crypto.verify(signature: result, message: Data(hexString: hashHexString)!, publicKey: publicKey.data))
         XCTAssertTrue(transactionToBeSigned.hasSignature)
-        XCTAssertEqual("e26f125d0e48e33efeb89efd616bfb7fb73e5a558f4ef8ac0094c3d7ad3a2796d5d8fc51fdaec40671bad3af21b21b4509961a8cf8b8f6b55239072d6956b40401", result.hexString)
+        XCTAssertEqual("e26f125d0e48e33efeb89efd616bfb7fb73e5a558f4ef8ac0094c3d7ad3a27962a2703ae02513bf98e452c50de4de4b9b118c259b68fa9866d99575f66df8d3d00", result.hexString)
     }
 }
+
+// https://api.tronscan.org/#/Transactions/findAll
+// https://tronscan.org/#/transaction/3d9fdb03efac1b2fa58cd92b7c283eb76a232cbc434790c8ca684255a7f212b6
+// GetTransactionById 3d9fdb03efac1b2fa58cd92b7c283eb76a232cbc434790c8ca684255a7f212b6
+/*
+ hash:
+ 8638224c089fa9671874b6caf9e0684c37d19bd199247c2091445cb1f75f0cf0
+ txid:
+ 3d9fdb03efac1b2fa58cd92b7c283eb76a232cbc434790c8ca684255a7f212b6
+ raw_data:
+ {
+ ref_block_bytes: b238
+ ref_block_hash: 3b15cb7e4a1c8168
+ contract:
+ {
+ contract 0 :::
+ [
+ contract_type: TransferContract
+ owner_address: TGQaQoCM7LprSH4TE4FemmZhEQMZMKvAGH
+ to_address: TKbhYo6a8nDjfVryNwh4oG5GA7kXfVSyqf
+ amount: 1100000
+ ]
+
+ }
+ timestamp: Thu Jan 01 10:00:00 AEST 1970
+ fee_limit: 0
+ }
+ signature:
+ {
+ signature 0 :e26f125d0e48e33efeb89efd616bfb7fb73e5a558f4ef8ac0094c3d7ad3a2796d5d8fc51fdaec40671bad3af21b21b4509961a8cf8b8f6b55239072d6956b40401
+ }
+ */
 
 class TronTransactionRawDataBuilder {
     var rawData = TronTransaction.RawData()
@@ -174,41 +176,5 @@ class TronTransactionContractBuiler {
 
     func build() -> TronTransaction.Contract {
         return contract
-    }
-}
-
-extension String {
-
-    /// Create `Data` from hexadecimal string representation
-    ///
-    /// This takes a hexadecimal representation and creates a `Data` object. Note, if the string has any spaces or non-hex characters (e.g. starts with '<' and with a '>'), those are ignored and only hex characters are processed.
-    ///
-    /// - returns: Data represented by this hexadecimal string.
-
-    func hexadecimal() -> Data? {
-        var data = Data(capacity: characters.count / 2)
-
-        let regex = try! NSRegularExpression(pattern: "[0-9a-f]{1,2}", options: .caseInsensitive)
-        regex.enumerateMatches(in: self, range: NSMakeRange(0, utf16.count)) { match, flags, stop in
-            let byteString = (self as NSString).substring(with: match!.range)
-            var num = UInt8(byteString, radix: 16)!
-            data.append(&num, count: 1)
-        }
-
-        guard data.count > 0 else { return nil }
-
-        return data
-    }
-}
-
-extension Data {
-
-    /// Create hexadecimal string representation of `Data` object.
-    ///
-    /// - returns: `String` representation of this `Data` object.
-
-    func hexadecimal() -> String {
-        return map { String(format: "%02x", $0) }
-            .joined(separator: "")
     }
 }
