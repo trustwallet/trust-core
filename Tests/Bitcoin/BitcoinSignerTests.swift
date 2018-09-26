c17e7c6536dae0210aa133dbc66501af8380b159
Alejandro Isaza <167236+alejandro-isaza@users.noreply.github.com>
2018-09-25 21:58:05 -0700

    Remove unnecessary lines

--- Tests/Bitcoin/BitcoinSignerTests.swift
+++ Tests/Bitcoin/BitcoinSignerTests.swift
@@ -67,9 +67,6 @@ class BitcoinSignerTests: XCTestCase {
         XCTAssertEqual(unsignedData.hexString, "0100000002fff7f7881a8099afa6940d42d1e7f6362bec38171ea3edf433541db4e4ad969f0000000000eeffffffef51e1b804cc89d182d279655c3aa89e815b1b309fe287d9b2b55d57b90ec68a0100000000ffffffff02202cb206000000001976a9148280b37df378db99f66f85c95a783a76ac7a6d5988ac9093510d000000001976a9143bde42dbee7e4dbe6a21b2d50ce2f0167faa815988ac11000000")
 
         let provider = BitcoinDefaultPrivateKeyProvider(keys: [utxoKey0, utxoKey1])
-        provider.keysByScriptHash = [
-            Data(hexString: "1d0f172a0ecb48aee1be1f2687d2963ae33f71a1")!: utxoKey1,
-        ]
         provider.scriptsByScriptHash = [
             Data(hexString: "1d0f172a0ecb48aee1be1f2687d2963ae33f71a1")!: script,
         ]
