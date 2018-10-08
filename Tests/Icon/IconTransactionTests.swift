2662547554edf8dace808b5ff0506800f0971afc
Christophe Wang <wchristophe2@gmail.com>
2018-10-08 12:52:41 +0900

    Update ICX sign test

--- Tests/Icon/IconTransactionTests.swift
+++ Tests/Icon/IconTransactionTests.swift
@@ -30,18 +30,18 @@ class IconTransactionTests: XCTestCase {
     }
 
     func testSignTransaction() {
-        let pKey = PrivateKey(data: Data(hexString: "a8697743afbded16c4b57829394e557e11f39164e15e5ae704210ed09c3f9857")!)!
+        let pKey = PrivateKey(data: Data(hexString: "a9d99d3a8d1e675e967b12e13ad5efe12d391325764bf9844a0ea48dfa663335")!)!
         var transaction: IconTransaction = IconTransaction(
-            from: IconAddress(string: "hx1d2b61e4bd800abecce8062281b46a03026e08fb")!,
-            to: IconAddress(string: "hx116f042497e5f34268b1b91e742680f84cf4e9f3")!,
-            value: BigInt("5000000000000000000"),
+            from: IconAddress(string: "hx9a4aa13be9f009a7ecce87ce0b81b8e922e97266")!,
+            to: IconAddress(string: "hxfc26e36379afbfc9626aac5e405bd9445bb12523")!,
+            value: BigInt("400000000000000000"),
             fee: BigInt("10000000000000000"),
-            timestamp: "1538939713000000",
+            timestamp: "1538970344000000",
             nonce: 8367273)
+
         transaction.hash()
         transaction.sign(privateKey: pKey)
-
-        XCTAssertEqual(transaction.tx_hash.hexString, "24c790715b220921e2ab7aa34173a24ead73f1b0f256093dc8e32755c27e061f")
-        XCTAssertEqual(transaction.signature.hexString, "a0a97e345281cb4c1b359ff1f62eee095632947f8b86dccc9eafbec3df9b93764988d455d2c4e7f61e3f63e13d220dbc042ce75eda49a8ff633bee4b6ac5248e01")
+        XCTAssertEqual(transaction.tx_hash.hexString, "7fda8288fbcddcb28984f0579379ab05a0a7b985bdf6dd349db73d1a55c6bacf")
+        XCTAssertEqual(transaction.signature.base64EncodedString(), "NzNzKhI3fVCWKpm5F7B8vZyJnEXun/nfXmVafoHEz0dnNoSNFoHxxjXWIJi2mMb9Ub/rxVGj3x5soYtP39QpzwA=")
     }
 }
