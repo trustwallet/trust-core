a5a8af1237bd5e790e4f4aa99c8866c3b5e61bd1
hewigovens <360470+hewigovens@users.noreply.github.com>
2018-11-06 09:50:03 +0800

    Fix review comments

--- Sources/Bitcoin/BitcoinUnspentSelector.swift
+++ Sources/Bitcoin/BitcoinUnspentSelector.swift
@@ -26,39 +26,32 @@ public struct BitcoinUnspentSelector {
             return ([], 0)
         }
 
+        // total values of utxos should be greater than targetValue
+        guard utxos.sum() >= targetValue && !utxos.isEmpty else {
+            throw BitcoinUnspentSelectorError.insufficientFunds
+        }
+
         // definitions for the following caluculation
         let doubleTargetValue = targetValue * 2
         var numOutputs = 2 // if allow multiple output, it will be changed.
         var numInputs = 2
-        var fee: Int64 {
-            return calculateFee(nIn: numInputs, nOut: numOutputs)
-        }
-        var targetWithFee: Int64 {
-            return targetValue + fee
-        }
-        var targetWithFeeAndDust: Int64 {
-            return targetWithFee + dustThreshold
-        }
 
         let sortedUtxos: [BitcoinUnspentTransaction] = utxos.sorted(by: { $0.output.value < $1.output.value })
 
-        // total values of utxos should be greater than targetValue
-        guard sortedUtxos.sum() >= targetValue && !sortedUtxos.isEmpty else {
-            throw BitcoinUnspentSelectorError.insufficientFunds
-        }
-
         // difference from 2x targetValue
         func distFrom2x(_ val: Int64) -> Int64 {
-            if val > doubleTargetValue { return val - doubleTargetValue } else { return doubleTargetValue - val }
+            return abs(val - doubleTargetValue)
         }
 
         // 1. Find a combination of the fewest outputs that is
         //    (1) bigger than what we need
         //    (2) closer to 2x the amount,
         //    (3) and does not produce dust change.
-        txN:do {
+        do {
             for numTx in (1...sortedUtxos.count) {
                 numInputs = numTx
+                let fee = calculateFee(nIn: numInputs, nOut: numOutputs)
+                let targetWithFeeAndDust = targetValue + fee + dustThreshold
                 let nOutputsSlices = sortedUtxos.eachSlices(numInputs)
                 var nOutputsInRange = nOutputsSlices.filter { $0.sum() >= targetWithFeeAndDust }
                 nOutputsInRange.sort { distFrom2x($0.sum()) < distFrom2x($1.sum()) }
@@ -69,9 +62,11 @@ public struct BitcoinUnspentSelector {
         }
 
         // 2. If not, find a combination of outputs that may produce dust change.
-        txDiscardDust:do {
+        do {
             for numTx in (1...sortedUtxos.count) {
                 numInputs = numTx
+                let fee = calculateFee(nIn: numInputs, nOut: numOutputs)
+                let targetWithFee = targetValue + fee
                 let nOutputsSlices = sortedUtxos.eachSlices(numInputs)
                 let nOutputsInRange = nOutputsSlices.filter {
                     return $0.sum() >= targetWithFee
